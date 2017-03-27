16f01a20fda5e31fe2c32440aeb6c1cb51e992df
xtianonil <co.tolentino@gmail.com>
2017-03-27 18:58:13 +0800

    first commit

--- www/js/group_admin.js
+++
@@ -1,3 +0,0 @@
-$(document).on('pagebeforeshow','#group_admin',function(){
-	$("[data-role=header]").hide();	//show burger icon
-});
--- www/js/pusher_init.js
+++
@@ -1,5 +0,0 @@
-var pusher = new Pusher('d13c29fea61746c0bf48', {
-    cluster: 'ap1',
-    encrypted: true
-    });
-var channel = pusher.subscribe('my-channel');
